a51d7230a487a21120e333188c616116f7c36c39
VaradhaQA <105559971+VaradhaQA@users.noreply.github.com>
2022-05-15 11:20:09 +0530

    Add files via upload

---
+++ MyCar.java
@@ -0,0 +1,15 @@
+package week1.day1;
+
+public class MyCar {
+
+	public static void main(String[] args) {
+	Car myBreak = new Car();
+    myBreak.applyBreak();
+    Car myGear = new Car();
+    myGear.applyGear();
+    Car myswitchonAc = new Car ();
+    myswitchonAc.switchonAc();
+    Car myapplyacclerate = new Car();
+    myapplyacclerate.applyAcclerate();
+	}
+}
